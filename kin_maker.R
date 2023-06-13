1e06dfd0e65b8f1ca360f431ec91f1128a6fe84a
Baber Ali <64258960+itsbaber@users.noreply.github.com>
2023-06-13 17:42:53 +0200

    Update kin_maker.R

--- kin_maker.R
+++ kin_maker.R
@@ -3,23 +3,23 @@ library(sommer)
 #####-----input ----####
 ########################
 ### read the map file
-map_v4 <- data.table::fread("~/work/AGPv4_Annotations/map_v4.txt")
+map_v4 <- data.table::fread("~/work/AGPv4_Annotations/map_v4.txt") ###this is the map file with atleast 4 columns, Chr, Start, End, and SNP_ID
 ### read the marker data
-markers <- readRDS("~/save/PhD_work/Genotypes/GenotypeMat.RDS")
-markers <- markers-1 #changing to -1, 0, and 1 as required by A.mat in the sommer package
+markers <- readRDS("~/save/PhD_work/Genotypes/GenotypeMat.RDS") ####marker data, needs to be in -1,0,1 for Sommer package. Can also be 0,1,2 if you decide to use a different package for kinship calculation
+#markers <- markers-1 #changing to -1, 0, and 1 as required by A.mat in the sommer package
 ### read evolutionary constraints data
-evo_cons <- read.csv("bloc_synthese2.csv",sep = ";")
-evo_cons$block_id_new <- paste0("Chr_",evo_cons$ChrName,"_block_",evo_cons$Bloc)
+evo_cons <- read.csv("bloc_synthese2.csv",sep = ";") #the dataframe that contains biological information
+evo_cons$block_id_new <- paste0("Chr_",evo_cons$ChrName,"_block_",evo_cons$Bloc) #only required if there are no IDs for the biological information
 ########################
 ####--setting loop params #####
 ########################
-loop.start <- 1
-loop.end <- nrow(evo_cons)
-selected.kinships <- list()
-remain.kinships <- list()
-no.snp.found <- vector()
-snp.count <- data.frame(matrix(data=NA, nrow=nrow(evo_cons),ncol=2))
-colnames(snp.count) <- c("Block","Count")
+loop.start <- 1  #if unchanged, it will go through every piece of biological information, but you can also choose a custom number to select only what interests you.
+loop.end <- nrow(evo_cons) #if unchanged, it will go through every piece of biological information, but you can also choose a custom number to select only what interests you.
+selected.kinships <- list() # list to store selected category kinships
+remain.kinships <- list() # list to store remaining category kinships
+no.snp.found <- vector() # the IDs of biological information for which no SNPs were found
+snp.count <- data.frame(matrix(data=NA, nrow=nrow(evo_cons),ncol=2)) # SNP count for the SNPs associated with biological information
+colnames(snp.count) <- c("Block","Count") #block is only the name of column where we will store the IDs of biological information
 #########################
 ########################
 ########################
